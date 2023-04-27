4efc4e96bb59feb8cefa747228c5b300d53d6aed
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2023-04-27 11:08:16 +0200

    add german samples

--- app/analysis/stats.py
+++ app/analysis/stats.py
@@ -1,22 +1,20 @@
 import pandas as pd
 from pathlib import Path
-def stats(dataset_path:str, prefix):
-    splits = [f'train_{prefix}.tsv', f'dev_{prefix}.tsv']
+from loguru import logger
 
-    for split in splits:
-        dataset_path = Path(dataset_path)
-        data = pd.read_csv(dataset_path/split, sep='\t')
-        print(f'Number of instances {len(data)} in {split}')
-        print(data.groupby(['label']).count())
-        if 'language' in data.columns:
-            print(data.groupby(['label', 'language']).count())
+logger.add(f"{__name__}.log", rotation="500 MB")
+
+
+def stats(dataset_path: str):
+
+    for file_path in dataset_path.rglob('*/*.tsv'):
+        logger.info(f'The stats for {file_path}')
+        data = pd.read_csv(file_path, sep='\t')
+        logger.info(f'Number of samples {len(data)}')
+        logger.info(data.groupby(['label'])['label'].count())
+
 
 if __name__ == '__main__':
-    # print('Stats for the multilingual dataset')
-    # stats(dataset_path='clef2023-checkthat-lab/task2/data/subtask-2-multilingual/processed', prefix='ml')
+    dataset_path = Path('dataset/clef2023')
 
-    print('Stats for the turkish dataset')
-    stats(dataset_path='clef2023-checkthat-lab/task2/data/subtask-2-turkish', prefix='tr')
-
-    print('Stats for the english dataset')
-    stats(dataset_path='clef2023-checkthat-lab/task2/data/subtask-2-english', prefix='en')
+    stats(dataset_path)
